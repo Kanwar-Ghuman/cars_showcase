4389df9d767479fcc9c7fc26c31c9327823795c9
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2023-06-11 20:42:37 -0500

    intial commit

---
+++ components/index.ts
@@ -0,0 +1,28 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
+import ShowMore from './ShowMore';
+
+
+
+CustomButton;
+
+
+export{
+   Hero,
+   CustomButton,
+   Navbar,
+   Footer,
+   SearchBar,
+   CustomFilter,
+   SearchManufacturer,
+   CarCard,
+   CarDetails,
+   ShowMore,
+}
